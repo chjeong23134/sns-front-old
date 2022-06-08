f599dfba00a658dc1488b3004265fa8b596e1c53
chjeong23134 <chjeong23134@gmail.com>
2022-06-08 11:33:21 +0900

    create sign in, sign up, nav publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.scss';
+import "./App.scss";
 
+import {Route, Routes} from "react-router-dom";
 
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
+
+import Nav from "./components/nav/Nav";
 
 export default function App() {
-	return (
-		<div className="app">
+    return (
+        <div className="app">
+            <Nav />
 
-		</div>
-	);
+            <div className="page">
+                <Routes>
+                    <Route path="sign-in" element={<SignIn />} />
+                    <Route path="sign-up" element={<SignUp />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,9 @@
+import "./Nav.scss";
+
+export default function Nav() {
+    return (
+        <div className="nav">
+            <p className="logo">SNS</p>
+        </div>
+    );
+}
